8d465cdbc4b9d661e6f5b1f5d76287abb35a85a0
OgulcanCulfa <ogulcanculfa@gmail.com>
2021-02-13 14:18:37 +0300

    Buffer saving option added

--- app.js
+++ app.js
@@ -1,38 +1,34 @@
-const imagemin = require('imagemin');
-const imageminJpegtran = require('imagemin-jpegtran');
-const imageminPngquant = require('imagemin-pngquant');
-const fs = require('fs');
+const imagemin = require("imagemin");
+const imageminJpegtran = require("imagemin-jpegtran");
+const imageminPngquant = require("imagemin-pngquant");
+const fs = require("fs");
 
 async function compress() {
+  try {
+    const file = await imagemin(["images/*.{jpg,png}"], {
+      destination: "compressed/",
+      plugins: [
+        imageminJpegtran(),
+        imageminPngquant({
+          quality: [0.6, 0.8],
+        }),
+      ],
+    });
 
-    try {
-        const file = await imagemin(['images/*.{jpg,png}'], {
-            destination: 'compressed/',
-            plugins: [
-                imageminJpegtran(),
-                imageminPngquant({
-                    quality: [0.6,0.8]
-                })
-            ]
-        })
+    const pth = file[0].destinationPath.toString().replace(/\\/g, "/");
+    const buff = file[0].data;
+    fs.readFile(pth, { encoding: "base64" }, (err, data) => {
+      //if (err) throw err;
 
-        const pth = file[0].destinationPath.toString().replace(/\\/g, "/");       
-        fs.readFile(pth,{encoding: 'base64'},(err,data) => {
-            if (err) throw err;
-            fs.writeFile("compressed.txt", data,() => {
+      // Save Buffer
+      //fs.writeFile("compressed_new.txt", buff, () => {});
 
-            })
-        });
-       
-    } catch(err) {
-        console.log(err);
-    }
+      // Save Base64
+      //fs.writeFile("compressed.txt", data, () => {});
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-
 compress();
-
-
-
-
-    
