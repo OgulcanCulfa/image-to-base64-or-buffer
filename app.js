62b89206a16c82a4379c855d3fa1fd3386df9fd7
OgulcanCulfa <ogulcanculfa@gmail.com>
2021-02-13 21:53:45 +0300

    small fixes

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const imageminPngquant = require("imagemin-pngquant");
 const fs = require("fs");
 
 async function compress() {
+
   try {
+
     const file = await imagemin(["images/*.{jpg,png}"], {
       destination: "compressed/",
       plugins: [
@@ -15,16 +17,17 @@ async function compress() {
       ],
     });
 
+
     const pth = file[0].destinationPath.toString().replace(/\\/g, "/");
     const buff = file[0].data;
+
+    // // Save as txt in Buffer format
+    // fs.writeFile("compressed_buffer.txt", buff, () => {});
+
     fs.readFile(pth, { encoding: "base64" }, (err, data) => {
-      //if (err) throw err;
-
-      // Save Buffer
-      //fs.writeFile("compressed_new.txt", buff, () => {});
-
-      // Save Base64
-      //fs.writeFile("compressed.txt", data, () => {});
+      if (err) throw err;
+      // Save as txt in Base64 format
+      //fs.writeFile("compressed_base64.txt", data, () => {});
     });
   } catch (err) {
     console.log(err);
